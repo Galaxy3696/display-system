67d89df271141255759ed63ad575e40142f573e0
msy <805109058@qq.com>
2023-01-31 16:23:58 +0800

    前端修改

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 from .models import Comment
-# class CommentForm(forms.Form):
-#     """创建留言框"""
-#     text = forms.CharField(label="评论内容", widget=forms.Textarea(attrs={
-#         'class': 'textarea is-radiusless is-shadowless', 'rows':6}))
 
 class CommentForm(forms.ModelForm):
     """创建留言框"""
--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('blog/<int:article_id>/', views.article, name="blog"),
     path('comment/<int:article_id>/<int:comment_id>/', views.article, name="comment"),
     path('dates/<int:year>/<int:month>/', views.blog_get_dates, name="dates"),   # 归档页
-    # path('tags/<int:tag_id>/', views.blog_get_tags, name="tags"),      # 文章标签列表页
 ]
